662265a020047f1d60eb5fe4b95f1932b40804d3
Jose Osuna <joseosuna.engineer@gmail.com>
2017-06-13 13:37:11 -0400

    B-1 adding jwt generation token.

--- auth/src/main/java/net/prottonne/lab/auth/controller/LoginController.java
+++ auth/src/main/java/net/prottonne/lab/auth/controller/LoginController.java
@@ -3,6 +3,15 @@
  */
 package net.prottonne.lab.auth.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.prottonne.lab.auth.entity.RequestLogin;
 import net.prottonne.lab.auth.entity.ResponseLogin;
 import org.springframework.http.MediaType;
@@ -23,9 +32,42 @@ public class LoginController {
     public ResponseLogin auth(@RequestBody RequestLogin requestLogin) {
         if ("user@email.net".equals(requestLogin.getEmail())
                 && "1234".equals(requestLogin.getPassword())) {
-            return new ResponseLogin("QpwL5tke4Pnpja7X");
+            return new ResponseLogin(generateJwtToken());
         }
 
         throw new RuntimeException("access denied");
+    }
+
+    private String generateJwtToken() {
+        try {
+            Date issue = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+            Date expiration = Date.from(LocalDateTime.now().plusHours(1L).atZone(ZoneId.systemDefault()).toInstant());
+            return Jwts.builder()
+                    .setId("ccbe3aa4-4278-4761-860e-805caca3fb64")
+                    .setIssuedAt(issue)
+                    .setExpiration(expiration)
+                    .claim("data", "8675A79C-6D07-4293-994B-E5510945CB66")
+                    .signWith(SignatureAlgorithm.HS256, "110A7B279F5D".getBytes("UTF-8"))
+                    .compact();
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private boolean validateJwtToken(String token) {
+        try {
+
+            Jwts.parser()
+                    .setSigningKey("110A7B279F5D".getBytes("UTF-8"))
+                    .parseClaimsJws(token);
+
+            return true;
+
+        } catch (SignatureException | UnsupportedEncodingException e) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, e);
+
+            return false;
+        }
     }
 }
