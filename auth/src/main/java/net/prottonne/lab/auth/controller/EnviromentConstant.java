2e51c083621ec7078123c64042ec4647f4fb0909
Jose Osuna <joseosuna.engineer@gmail.com>
2017-06-17 10:30:18 -0400

    B-1 refactoring auth.

---
+++ auth/src/main/java/net/prottonne/lab/auth/controller/EnviromentConstant.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2017 Prottonne
+ */
+package net.prottonne.lab.auth.controller;
+
+/**
+ *
+ * @author Jose Osuna
+ */
+public enum EnviromentConstant {
+    JWT_SECRET("WWW_1232");
+
+    private final String value;
+
+    private EnviromentConstant(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
