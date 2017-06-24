ef50242d9a746a40cf915c3507d9ff33ca203f61
Jose Osuna <jose.osuna@prottonne.org>
2017-06-23 23:09:55 -0400

    B-1 adding service registry.

--- src/main/java/net/prottonne/lab/auth/Main.java
+++ src/main/java/net/prottonne/lab/auth/Main.java
@@ -5,11 +5,13 @@ package net.prottonne.lab.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  *
  * @author Jose Osuna
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Main {
 
